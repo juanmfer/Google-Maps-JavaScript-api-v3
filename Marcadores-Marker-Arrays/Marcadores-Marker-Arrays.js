21f9ec9fcd85fdf2217cf0f2270a2bec84bf823c
Juan Manuel <fernandezjuanm@gmail.com>
2023-10-02 08:50:39 -0300

    Update Marcadores-Marker-Arrays.js

--- Marcadores-Marker-Arrays/Marcadores-Marker-Arrays.js
+++ Marcadores-Marker-Arrays/Marcadores-Marker-Arrays.js
@@ -8,7 +8,7 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4, // si el zoom es mayor, mas se acercara al marcador del center.
         center: { lat:-34.60286907260642, lng:-58.381370076086085 }, // center  es donde se centrara el mapa, puede ser una constante, variable, o directamente latitud y longitud.
-      });
+    });
 
     let latlongArray = [ // array con nuestros puntos de interes
         { nombre: "Marcador Buenos Aires", lat:-34.60286907260642, lng:-58.381370076086085}, // objetos del arreglo
@@ -19,12 +19,12 @@ function initMap() {
     ]
 
     for(let enarray of latlongArray) { // recorremos el array
-        new google.maps.Marker({ // Creamos el marcador en la posicion enarray 
+        new google.maps.Marker({ // Creamos el marcador en la posicion enarray
             position: { lat: enarray.lat, lng: enarray.lng }, // asignamos la posicion del marcador.
             map: map,  // donde mostraremos el marcador, el mapa esta creado en la constante map.
                        // se puede crear como solo "map,"  o  "map: map,", aqui el ejemplo.
             title: enarray.nombre, // asignamos el objeto nombre de nuestro array.
-          });
+        });
     }
 }
 window.initMap = initMap;
